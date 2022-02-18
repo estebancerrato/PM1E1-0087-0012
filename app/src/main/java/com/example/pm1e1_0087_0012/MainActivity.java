7d0ac6a319db54b49d4fb831934bf6fbd890edbc
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2022-02-18 00:10:00 -0600

    variable casteada foto

--- app/src/main/java/com/example/pm1e1_0087_0012/MainActivity.java
+++ app/src/main/java/com/example/pm1e1_0087_0012/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.pm1e1_0087_0012;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 }
