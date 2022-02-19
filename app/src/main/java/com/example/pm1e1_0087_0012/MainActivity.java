6f8207343b7626658da1f2734cd606891ac18e00
estebancerrato <estebancerrato@hotmail.com>
2022-02-18 21:08:35 -0600

    floating y ir a la siguiente ventana de agregar pais

--- app/src/main/java/com/example/pm1e1_0087_0012/MainActivity.java
+++ app/src/main/java/com/example/pm1e1_0087_0012/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.pm1e1_0087_0012;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatAgregarPais);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Ir a la ventaja de ingresar nuevo pais
+                Intent intent = new Intent(getApplicationContext(),ActivityPais.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
